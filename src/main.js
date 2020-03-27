ce78ed6d5f1cf86f5dc0dcdcc38389279ae5ab29
lsnjie <1339876137@qq.com>
2020-03-27 15:14:18 +0800

    完成项目的搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入 router
+import router from './router/index'
+// 导入element-ui
+import Element from 'element-ui'
+// 导入 element样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 使用element
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+// 导入  Vue
+import Vue from 'vue'
+// 导入VueRouter
+import VurRouter from 'vue-router'
+import VueRouter from 'vue-router'
+
+// 注册Vue
+Vue.use(VurRouter)
+
+// 实列化路由
+const router = new VueRouter({
+    router: {
+
+    }
+})
+
+export default router
